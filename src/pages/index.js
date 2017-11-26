97b09be7e324b5b90c1193687e845f53072a4bb9
zehao wu <wuzehao16@qq.com>
2017-11-26 11:45:57 +0800

    Add material-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,13 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p style={{color:`blue`}}>Welcome to your new Gatsby site.</p>
+    <p style={{color:`tomato`}}>Now go build something great.</p>
+    <img src="http://lorempixel.com/400/200/" alt="" />
     <Link to="/page-2/">Go to page 2</Link>
+    <div>
+     <Link to="/counter/">Counter</Link>
+    </div>
   </div>
 )
 
